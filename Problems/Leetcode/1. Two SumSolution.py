11cf5e511424d7e0cdb582792788057902ed5b04
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-27 15:32:47 +0200

    Update 1. Two SumSolution.py

--- Problems/Leetcode/1. Two SumSolution.py
+++ Problems/Leetcode/1. Two SumSolution.py
@@ -1,9 +1,8 @@
 
 
 
-
-
-/*
+  """
+  
   Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
   You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -34,7 +33,7 @@
   -109 <= target <= 109 
   
   27-January-2023
-*/
+"""
 
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
