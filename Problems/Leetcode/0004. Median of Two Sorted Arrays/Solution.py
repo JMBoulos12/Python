798725b678dd7db90948fce2aadc3e1a4f1665f3
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-06-05 11:01:49 +0300

    Update Solution.py

--- Problems/Leetcode/0004. Median of Two Sorted Arrays/Solution.py
+++ Problems/Leetcode/0004. Median of Two Sorted Arrays/Solution.py
@@ -34,3 +34,7 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         arr = sorted([*nums1, *nums2])
         return arr[len(arr) // 2] if len(arr) % 2 else (arr[(len(arr) // 2) - 1] + arr[len(arr) // 2]) / 2
+
+      
+      
+      
